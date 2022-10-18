e3da03b45b618840511675c4d74570a3fc057324
coreyalmon <59987216+coreyalmon@users.noreply.github.com>
2022-10-17 20:00:48 -0400

    Update WideWorldImportersQS.java

--- src/WideWorldImportersQS.java
+++ src/WideWorldImportersQS.java
@@ -1,4 +1,16 @@
+/**
+ * 	WideWorldImportersQS Selects a query to execute from WideWorldImporters database.
+ *  @author Almon
+ */
+public class WideWorldImportersQS extends QuerySelector {
+	/**
+	 * constructor for WideWorldImportersQS that calls QuerySelector's constructor
+	 * @param con A Connector type for already established connection
+	 * @param query A String type for querying a database
+	 */
+	public WideWorldImportersQS(Connector con, String query) {
+		super(con, query);
+	}
 
-public class WideWorldImportersQS {
 
 }
