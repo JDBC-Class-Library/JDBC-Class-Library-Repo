9a9078751190bdd82fd5e1223a8b4ed1048c9459
coreyalmon <59987216+coreyalmon@users.noreply.github.com>
2022-10-17 20:01:53 -0400

    Update WideWorldImportersConnector.java

--- src/WideWorldImportersConnector.java
+++ src/WideWorldImportersConnector.java
@@ -1,4 +1,22 @@
-
-public class WideWorldImportersConnector {
-
+/**
+ * Connector for WideWorldImporters database
+ * @author Almon
+ */
+public class WideWorldImportersConnector extends Connector {
+	/**
+	 * Constructor that passes specific database name to Connector
+	 */
+	public WideWorldImportersConnector() {
+		super("WideWorldImporters;");
+		
+	}
+	
+	/**
+	 * Prints database name
+	 * @return void
+	 */
+	@Override
+	void printDatabaseName() {
+		System.out.println("WideWorldImporters");
+	}
 }
